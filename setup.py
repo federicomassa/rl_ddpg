773034714aa219860c62bcae0762df32fc546425
Federico Massa <fedemassa91@gmail.com>
2019-12-12 10:33:26 +0100

    Testing setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='rl_ddpg',
+   version='0.1',
+   description='A basic implementation of the DDPG algorithm',
+   author='Federico Massa',
+   author_email='fedemassa91@gmail.com',
+   packages=['rl_ddpg'],
+   install_requires=['tensorflow'], 
+)
